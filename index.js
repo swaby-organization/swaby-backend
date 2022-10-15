532ecfd13f333409c43bb3a9d06c68491d2fc986
Zaid alshibi <zaidealshibi@gmail.com>
2022-10-15 13:12:45 +0300

    created the user model and connected the database

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 'use strict';
 
 require( 'dotenv' ).config();
+const { db } = require( './DatabaseModels' );
 const server = require( './server' );
 
-
-server.start( process.env.PORT || 3001 );
+db.sync().then( () => {
+    server.start( process.env.PORT || 3001 );
+} ).catch( ( err ) => {
+    console.log( `Database connection error : ${err}` );
+} );
