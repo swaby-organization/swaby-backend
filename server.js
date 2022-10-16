5637441c387184dbc8462c5ce031527c4b4fd8d6
user name <user email>
2022-10-16 19:47:26 +0000

    adding the error handler middleware to the backend size

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const express = require( 'express' );
 const cors = require( 'cors' );
 const app = express();
+const errorHandler404= require('./error-handlers/404');
+const errorHandler500=require ('./error-handlers/500');
 
 app.use( cors() );
 app.use( express.json() );
@@ -16,6 +18,8 @@ const start = ( port ) => {
     app.listen( port, () => console.log( `Listening on port ${port}` ) );
 };
 
+app.use(errorHandler404);
+app.use(errorHandler500);
 module.exports = {
     app,
     start
