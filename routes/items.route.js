ad209265668d4f757220a5faeecfd23fbaa48e1b
Ahmed AL Awamleh <awamlehad@icloud.com>
2022-10-20 02:59:50 +0300

    add test routs for item

--- routes/items.route.js
+++ routes/items.route.js
@@ -1,16 +1,16 @@
 'use strict';
 
-const express = require( 'express' );
-const { getAllItems, getItemById, getItemsByUser, createItem, updateItem } = require( '../controllers/item.controller' );
-const { uploadItemImages } = require( '../upload/uploadItemImages' );
+const express = require('express');
+const { getAllItems, getItemById, getItemsByUser, createItem, updateItem } = require('../controllers/item.controller');
+const { uploadItemImages } = require('../upload/uploadItemImages');
 const router = express.Router();
 
 
-router.get( '/items', getAllItems );
-router.get( '/items/:id', getItemById );
-router.get( '/itemsbyuser/:userid', getItemsByUser );
-router.post( '/items', uploadItemImages.array( "uploadedImages", 4 ), createItem );
-router.post( '/items/:id', uploadItemImages.array( "uploadedImages", 4 ),updateItem );
+router.get('/items', getAllItems);
+router.get('/items/:id', getItemById);
+router.get('/itemsbyuser/:userid', getItemsByUser);
+router.post('/items', uploadItemImages.array("uploadedImages", 4), createItem);
+router.post('/items/:id', uploadItemImages.array("uploadedImages", 4), updateItem);
 
 module.exports = router;
 
