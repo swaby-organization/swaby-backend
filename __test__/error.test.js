5a4568d529a7d3ae62e264cc91973e7fd3086d5b
user name <user email>
2022-10-25 02:52:49 +0000

    editing error handler test

--- __test__/error.test.js
+++ __test__/error.test.js
@@ -7,10 +7,17 @@ describe('Testing Errors (404, 401)', () => {
         const res = await request.get('/myItems');
         console.log("=============================================")
         expect(res.status).toEqual(404);
-    }),
+        expect(error.message).toBe("no password given");
+    })
 
-        it('Testing 401 error',async () => {
-            const res = await request.get('/userinfo/1');
-            expect(res.status).toEqual(401);
-        })
+    it('Testing 401 error', async () => {
+        const res = await request.get('/userinfo/1');
+        expect(res.status).toEqual(401);
+        expect(error.message).toBe(expect(error.message)); 
+    })
+    it('Testing 500 error', async () => {
+        const res = await request.get('/userinfo');
+        expect(res.status).toEqual(500);
+        expect(error.message).toBe("Server Error"); 
+    })
 })
